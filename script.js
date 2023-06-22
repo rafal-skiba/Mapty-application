5499137584f24524cfea302117959dc6852cb42c
Rafał Skiba <rr.skiba92@gmail.com>
2023-06-22 22:59:22 +0200

    map with user location added to app

--- script.js
+++ script.js
@@ -10,3 +10,27 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation) {
+navigator.geolocation.getCurrentPosition(function(position) {
+const {latitude} = position.coords;
+const {longitude} = position.coords;
+// const coords = [latitude, longitude]
+
+const map = L.map('map').setView([latitude, longitude], 17);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([latitude, longitude]).addTo(map)
+    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+    .openPopup();
+
+
+}, function() {
+alert('could not get your postion')
+})
+
+}
+
