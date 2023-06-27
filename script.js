288dca7dd0ff7de86bdee6a1d15b2274429175b7
Rafał Skiba <rr.skiba92@gmail.com>
2023-06-27 22:30:38 +0200

    code architectured

--- script.js
+++ script.js
@@ -13,32 +13,52 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 let map, mapEvent;
 
-if (navigator.geolocation) {
-navigator.geolocation.getCurrentPosition(function(position) {
-const {latitude} = position.coords;
-const {longitude} = position.coords;
-// const coords = [latitude, longitude]
+class App {
+    constructor() {
+        this._getPostion();
+    }
 
-map = L.map('map').setView([latitude, longitude], 17);
+    _getPostion(){
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(this._loadMap, function() {
+            alert('could not get your postion')
+            })
+            
+            }
+    }
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-
-
-map.on('click', function (mapE) {
-mapEvent = mapE;
-form.classList.remove('hidden');
-inputDistance.focus();
-    
-})
-
-}, function() {
-alert('could not get your postion')
-})
-
+    _loadMap(position){
+        const {latitude} = position.coords;
+        const {longitude} = position.coords;
+        // const coords = [latitude, longitude]
+        
+        map = L.map('map').setView([latitude, longitude], 17);
+        
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+        
+        
+        
+        map.on('click', function (mapE) {
+        mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();    
+        });
+        
 }
+
+    _showForm(){}
+
+    _toggleElevationField() {}
+
+    _newWorkout() {}
+}
+
+const app = new App();
+
+
+
 
 form.addEventListener('submit', function (e) {
 
