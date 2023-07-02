d08fa64c5d0b503d42d0c4de4c15b70e55b204df
Rafał Skiba <rr.skiba92@gmail.com>
2023-07-02 18:25:37 +0200

    Class Running and Cycling created

--- script.js
+++ script.js
@@ -11,77 +11,125 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map, mapEvent;
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
 
-class App {
-    #map;
-    #mapEvent;
-    constructor() {
-        this._getPostion();
-
-        form.addEventListener('submit', this._newWorkout.bind(this))
-            
-            inputType.addEventListener('change', function () {
-               inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-               inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-            })
-    }
-
-    _getPostion(){
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), function() {
-            alert('could not get your postion')
-            })
-            
-            }
-    }
-
-    _loadMap(position){
-        const {latitude} = position.coords;
-        const {longitude} = position.coords;
-        // const coords = [latitude, longitude]
-        
-        this.#map = L.map('map').setView([latitude, longitude], 17);
-        
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(this.#map);
-        
-        
-        
-        this.#map.on('click', this._showForm.bind(this));
-        
+  constructor(coords, distance, duration) {
+    // this.date =
+    // this.id =
+    this.coords = coords;
+    this.distance = distance;
+    this.duration = duration;
+  }
 }
 
-    _showForm(mapE){
-        this.#mapEvent = mapE;
-        form.classList.remove('hidden');
-        inputDistance.focus();    
-    }
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords,distance,duration);
+    this.cadence = cadence;
+    this.clacPace();
+  }
 
-    _toggleElevationField() {}
+  clacPace() {
+    //min/kim
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
 
-    _newWorkout(e) {
-        e.preventDefault();
-      
-        inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
-        const {lat, lng} = this.#mapEvent.latlng;
-            L.marker([lat, lng])
-            .addTo(this.#map)
-            .bindPopup(L.popup({
-                maxWidth: 250,
-                minWidth: 100,
-                autoClose: false,
-                closeOnClick: false,
-                className: 'running-popup'
-            }))
-            .setPopupContent('working in progress')
-            .openPopup();
+class Cycling extends Workout {
+    constructor(coords, distance, duration, elevationGain) {
+        super(coords,distance,duration);
+        this.elevationGain = elevationGain;
+        this.clacSpeed();
+      }
+
+      clacSpeed() {
+        //min/kim
+        this.speed = this.distance / (this.duration/60);
+        return this.speed;
+      }
+}
+
+
+
+const run1 = new Running([39, -12], 5, 24, 178)
+const cycling1 = new Cycling([39, -12], 27, 60, 523)
+console.log(run1, cycling1)
+//App architecture
+
+class App {
+  #map;
+  #mapEvent;
+  constructor() {
+    this._getPostion();
+
+    form.addEventListener('submit', this._newWorkout.bind(this));
+
+    inputType.addEventListener('change', this._toggleElevationField);
+  }
+
+  _getPostion() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert('could not get your postion');
+        }
+      );
     }
+  }
+
+  _loadMap(position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    // const coords = [latitude, longitude]
+
+    this.#map = L.map('map').setView([latitude, longitude], 17);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    this.#map.on('click', this._showForm.bind(this));
+  }
+
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
+
+  _toggleElevationField() {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+
+  _newWorkout(e) {
+    e.preventDefault();
+
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        '';
+    const { lat, lng } = this.#mapEvent.latlng;
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup',
+        })
+      )
+      .setPopupContent('working in progress')
+      .openPopup();
+  }
 }
 
 const app = new App();
-
-
-
-
