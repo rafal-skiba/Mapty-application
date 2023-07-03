cb768749e567ee8a045f4f29bdc997ae9cd5ce0c
Rafał Skiba <rr.skiba92@gmail.com>
2023-07-03 22:11:14 +0200

    zoom animation added to map

--- script.js
+++ script.js
@@ -92,6 +92,7 @@ class App {
     this._getPostion();
     form.addEventListener('submit', this._newWorkout.bind(this));
     inputType.addEventListener('change', this._toggleElevationField);
+    containerWorkouts.addEventListener('click', this._moveToPopUp.bind(this));
   }
 
   _getPostion() {
@@ -276,6 +277,19 @@ class App {
     }
 
     form.insertAdjacentHTML('afterend', html);
+  }
+
+  _moveToPopUp(e) {
+    const workoutEl = e.target.closest('.workout');
+    if (!workoutEl) return;
+
+    const workout = this.#workouts.find(
+      work => work.id === workoutEl.dataset.id
+    );
+    this.#map.setView(workout.coords, 13, {
+      animate: true,
+      pan: { duration: 1 },
+    });
   }
 }
 
